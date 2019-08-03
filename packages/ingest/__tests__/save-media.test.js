1a24ef6abec2c71faa04db8fc2d444529bab93a2
Tom Pallister <tom@threemammals.com>
2019-08-03 14:35:44 +0100

    test

--- packages/ingest/__tests__/save-media.test.js
+++ packages/ingest/__tests__/save-media.test.js
@@ -5,36 +5,35 @@ const { saveMedia } = require('../src/save-media');
 
 let app;
 let server;
+const pathThatDoesExist = './__tests__/image_tests/test-image.png';
+const pathThatDoesntExist = './__tests__/a/b/c/test-image.png';
+
+beforeAll(() => {
+  app = express();
+
+  const dir = path.join(__dirname, 'test-data');
+
+  app.use(express.static(dir));
+
+  server = app.listen(3123, () => {
+    console.log('Listening on http://localhost:3123/');
+  });
+});
+
+afterAll((done) => {
+  server.close(() => {
+    done();
+    // console.log('CLOSING CONNECTION');
+  });
+  fs.unlinkSync(pathThatDoesExist);
+  fs.unlinkSync(pathThatDoesntExist);
+  // fs.unlinkSync('./__tests__/a/b/c/');
+  // fs.unlinkSync('./__tests__/a/b/');
+  // fs.unlinkSync('./__tests__/a/');
+  // fs.unlinkSync('./__tests__/');
+});
 
 describe('saveMedia', () => {
-  const pathThatDoesExist = './__tests__/image_tests/test-image.png';
-  const pathThatDoesntExist = './__tests__/a/b/c/test-image.png';
-
-  beforeAll(() => {
-    app = express();
-
-    const dir = path.join(__dirname, 'test-data');
-
-    app.use(express.static(dir));
-
-    server = app.listen(3123, () => {
-      console.log('Listening on http://localhost:3123/');
-    });
-  });
-
-  afterAll(() => {
-    fs.unlinkSync(pathThatDoesExist);
-    fs.unlinkSync(pathThatDoesntExist);
-    fs.unlinkSync('./__tests__/a/b/c/');
-    fs.unlinkSync('./__tests__/a/b/');
-    fs.unlinkSync('./__tests__/a/');
-    fs.unlinkSync('./__tests__/');
-    console.log('CLOSING CONNECTION');
-    server.close(() => {
-      console.log('Http server closed.');
-    });
-  });
-
   it('should save image', async () => {
     const expected = fs.readFileSync('./__tests__/test-data/test-image.png');
     const src = 'http://localhost:3123/test-image.png';
