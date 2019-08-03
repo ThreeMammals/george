0a605f630ff1ec8e4f49e1833386b2796a2fbcf7
Tom Pallister <tom@threemammals.com>
2019-08-03 11:20:31 +0100

    test

--- packages/scripts/build.sh
+++ packages/scripts/build.sh
@@ -3,7 +3,6 @@
 docker run --rm \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     --mount type=bind,source=$(which docker),target=$(which docker) \
-    -v "${HOME}/.docker/config.json:/root/.docker/config.json" \
     -v "${HOME}/.npm:/root/.npm" \
     -v ${HOME}/.ssh:/root/.ssh \
     -v "$(pwd):/code" \
