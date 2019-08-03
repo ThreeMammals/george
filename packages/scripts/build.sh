078ae3d1eda4f2087f644b31b111865d253a9932
Tom Pallister <tom@threemammals.com>
2019-08-03 10:31:45 +0100

    test

--- packages/scripts/build.sh
+++ packages/scripts/build.sh
@@ -1,17 +1,3 @@
-# Get the packages that have changed
-# changed_packages=$(echo "{$(lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | join(",")'),}")
-
-changed_packages={@george/components,}
-
-echo "changed_packages=${changed_packages}"
-
-if [ ${changed_packages} = "{,}" ] || [ ${changed_packages} = "{}" ] || [ ${changed_packages} = {} ]
-then
-  echo "No packages were changed, nothing to build....if there was something to build put it in a package!!"
-  exit 0
-fi
-
-# Build, publish and deploy the packages that have changed.
 docker run --rm \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     --mount type=bind,source=$(which docker),target=$(which docker) \
@@ -21,17 +7,4 @@ docker run --rm \
     -v "$(pwd):/code" \
     -w "/code" \
     george/build \
-    /bin/bash -c \
-    "git config --global user.email \"tom@threemammals.com\" && \
-    git config --global user.name \"Tom Pallister\" && \
-    npm install && \
-    # bootstrap only the packages we need to build
-    lerna bootstrap --no-ci --scope=${changed_packages} && \
-    # do all the versioning and push the tags back to the remote
-    lerna version --conventional-commits --changelog-preset angular --yes
-    # # run all the tests
-    # lerna run --scope=${changed_packages} --stream --concurrency=1 test && \
-    # # build all the packages that have changed and any deps..for docker containers this should build them for node packages it will push them to npm or private repo
-    # lerna run --scope=${changed_packages} --stream --concurrency=1 build-ci && \
-    # # publish all the packages that have changed
-    # lerna run --scope=${changed_packages} --stream --concurrency=1 publish"
+    ./packages/scripts/thing.sh
---
+++ packages/scripts/thing.sh
@@ -0,0 +1,27 @@
+git config --global user.email "tom@threemammals.com"
+git config --global user.name "Tom Pallister"
+
+# Get the packages that have changed
+# changed_packages=$(echo "{$(lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | join(",")'),}")
+
+changed_packages={@george/components,}
+
+echo "changed_packages=${changed_packages}"
+
+if [ ${changed_packages} = "{,}" ] || [ ${changed_packages} = "{}" ] || [ ${changed_packages} = {} ]
+then
+  echo "No packages were changed, nothing to build....if there was something to build put it in a package!!"
+  exit 0
+fi
+
+npm install 
+# bootstrap only the packages we need to build
+lerna bootstrap --no-ci --scope=${changed_packages}
+# do all the versioning and push the tags back to the remote
+lerna version --conventional-commits --changelog-preset angular --yes
+# # # run all the tests
+# lerna run --scope=${changed_packages} --stream --concurrency=1 test
+# # # build all the packages that have changed and any deps..for docker containers this should build them for node packages it will push them to npm or private repo
+# lerna run --scope=${changed_packages} --stream --concurrency=1 build-ci
+# # # publish all the packages that have changed
+# lerna run --scope=${changed_packages} --stream --concurrency=1 publish
