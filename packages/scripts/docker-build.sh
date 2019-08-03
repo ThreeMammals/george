5c015926cdef69a2435a021528425f6e6c1b9204
Tom Pallister <tom@threemammals.com>
2019-08-03 16:03:40 +0100

    test

--- packages/scripts/docker-build.sh
+++ packages/scripts/docker-build.sh
@@ -4,7 +4,6 @@ docker run --rm \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
     --mount type=bind,source=$(which docker),target=$(which docker) \
     -v "${HOME}/.npm:/root/.npm" \
-    # -v ${HOME}/.ssh:/root/.ssh \
     -v "$(pwd):/code" \
     -w "/code" \
     -e GEORGE_NPMRC=${GEORGE_NPMRC} \
