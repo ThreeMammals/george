9e4e384d840211ada14950af9edaa146ded8b837
Tom Pallister <tom@threemammals.com>
2019-08-03 11:21:29 +0100

    test

--- packages/scripts/build.sh
+++ packages/scripts/build.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-docker run --rm \
-    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-    --mount type=bind,source=$(which docker),target=$(which docker) \
-    -v "${HOME}/.npm:/root/.npm" \
-    -v ${HOME}/.ssh:/root/.ssh \
-    -v "$(pwd):/code" \
-    -w "/code" \
-    -e GEORGE_NPMRC=${GEORGE_NPMRC} \
-    -e GEORGE_ACR_LOGIN_NAME=${GEORGE_ACR_LOGIN_NAME} \
-    -e GEORGE_ACR_URL=${GEORGE_ACR_URL} \
-    -e GEORGE_ACR_USERNAME=${GEORGE_ACR_USERNAME} \
-    -e GEORGE_ACR_PASSWORD=${GEORGE_ACR_PASSWORD} \
-    george/build \
-    ./packages/scripts/thing.sh
+git config --global user.email "tom@threemammals.com"
+git config --global user.name "Tom Pallister"
+
+# Get the packages that have changed
+changed_packages=$(echo "{$(lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | join(",")'),}")
+
+echo "changed_packages=${changed_packages}"
+
+if [ ${changed_packages} = "{,}" ] || [ ${changed_packages} = "{}" ] || [ ${changed_packages} = {} ]
+then
+  echo "No packages were changed, nothing to build....if there was something to build put it in a package!!"
+  exit 0
+fi
+
+npm install 
+# bootstrap only the packages we need to build
+lerna bootstrap --no-ci --scope=${changed_packages}
+# do all the versioning and push the tags back to the remote
+lerna version --conventional-commits --changelog-preset angular --yes
+# run all the tests
+lerna run --scope=${changed_packages} --stream --concurrency=1 test
+# build all the packages that have changed and any deps..for docker containers this should build them for node packages it will push them to npm or private repo
+lerna run --scope=${changed_packages} --stream --concurrency=1 build-ci
+# publish all the packages that have changed
+lerna run --scope=${changed_packages} --stream --concurrency=1 publish
---
+++ packages/scripts/docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+docker run --rm \
+    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+    --mount type=bind,source=$(which docker),target=$(which docker) \
+    -v "${HOME}/.npm:/root/.npm" \
+    -v ${HOME}/.ssh:/root/.ssh \
+    -v "$(pwd):/code" \
+    -w "/code" \
+    -e GEORGE_NPMRC=${GEORGE_NPMRC} \
+    -e GEORGE_ACR_LOGIN_NAME=${GEORGE_ACR_LOGIN_NAME} \
+    -e GEORGE_ACR_URL=${GEORGE_ACR_URL} \
+    -e GEORGE_ACR_USERNAME=${GEORGE_ACR_USERNAME} \
+    -e GEORGE_ACR_PASSWORD=${GEORGE_ACR_PASSWORD} \
+    george/build \
+    ./packages/scripts/build.sh
