eb5ceca9a1c65d27c295660c4ef4262da122b45b
Tom Pallister <tom@threemammals.com>
2019-08-04 09:34:37 +0100

    test

--- packages/scripts/docker-build-pr.sh
+++ packages/scripts/docker-build-pr.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-echo $CIRCLE_BRANCH
-
 # lerna needs these to push tags to remote
 git config --global user.email "tom@threemammals.com"
 git config --global user.name "Tom Pallister"
+
+# do the versioning but don't push it back to the remote
+lerna version prerelease --conventional-commits --no-changelog --yes
 
 # Get the packages that have changed
 changed_packages=$(echo "{$(lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | join(",")'),}")
@@ -16,9 +17,6 @@ then
   echo "No packages were changed, nothing to build....if there was something to build put it in a package!!"
   exit 0
 fi
-
-# do the versioning but don't push it back to the remote
-# lerna version prerelease --conventional-commits --no-changelog --no-git-tag-version --no-push --yes
 
 docker run --rm \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
--- packages/scripts/docker-build.sh
+++ packages/scripts/docker-build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo $CIRCLE_BRANCH
-
 # lerna needs these 
 git config --global user.email "tom@threemammals.com"
 git config --global user.name "Tom Pallister"
+
+# do all the versioning and push the tags back to the remote
+# we do this before the docker container because we need the ssh key for it and 
+# I cant get the ssh key to work in the container on circlci
+lerna version --conventional-commits --changelog-preset angular --yes
 
 # Get the packages that have changed
 changed_packages=$(echo "{$(lerna changed --json --loglevel=silent | jq -c -r 'map(.name) | join(",")'),}")
@@ -16,11 +19,6 @@ then
   echo "No packages were changed, nothing to build....if there was something to build put it in a package!!"
   exit 0
 fi
-
-# do all the versioning and push the tags back to the remote
-# we do this before the docker container because we need the ssh key for it and 
-# I cant get the ssh key to work in the container on circlci
-lerna version --conventional-commits --changelog-preset angular --yes
 
 docker run --rm \
     --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
