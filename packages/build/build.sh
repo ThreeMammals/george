cfe7e1fe8b8f706fc0737bfceb6b13b413b55e9d
Tom Pallister <tom@threemammals.com>
2019-08-22 22:21:50 +0100

    version build image

--- packages/build/build.sh
+++ packages/build/build.sh
@@ -1,3 +1,5 @@
 docker build -t mijitt0m/build .
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+docker tag mijitt0m/build mijitt0m/build:1.0.0
 docker push mijitt0m/build:latest
+docker push mijitt0m/build:1.0.0
