3b2e2c7dd6781e37404b3512bd0820144ceb4cd1
Tom Pallister <tom@threemammals.com>
2019-08-22 21:31:38 +0100

    dirty hacking

--- packages/build/push.sh
+++ packages/build/push.sh
@@ -1,1 +1,1 @@
-echo $GEORGE_DOCKER_PASSWORD | docker login --username $GEORGE_DOCKER_USERNAME --password-stdin  && docker push mijitt0m/build:latest
+echo $GEORGE_DOCKER_PASSWORD | docker login -u $GEORGE_DOCKER_USERNAME --password-stdin  && docker push mijitt0m/build:latest
