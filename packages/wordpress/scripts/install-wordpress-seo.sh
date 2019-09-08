87013f0c3644a63b82249db7c5decc2da152889c
TomPallister <tom@threemammals.com>
2019-09-08 11:14:06 +0100

    install yoast

---
+++ packages/wordpress/scripts/install-wordpress-seo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Installing Yoast"
+
+if [ -d "src/wp-content/plugins/wordpress-seo" ]
+then
+    echo "Repo cloned already"
+    cd src/wp-content/plugins/wordpress-seo
+    composer install
+    yarn
+    grunt build
+else
+    echo "Cloning repo"
+    cd src/wp-content/plugins/
+    git clone https://github.com/Yoast/wordpress-seo.git
+    cd wordpress-seo
+    composer install
+    yarn
+    grunt build
+fi
+
+echo "Finished installing Yoast"
+
+
+
